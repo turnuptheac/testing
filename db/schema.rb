e6a78d65ce17b698b8f5794ba59413380a841aff
Ethan Truong <etruong42@gmail.com>
2016-11-13 00:19:29 -0500

    switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161110033553) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flashcards", force: :cascade do |t|
     t.integer  "bin"
     t.string   "question"
